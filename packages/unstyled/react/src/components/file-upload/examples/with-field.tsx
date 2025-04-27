b8316139457a7f6c8dfa9e02a19d393c5a40119a
peterjaberau <peterjaberau@gmail.com>
2025-04-27 23:58:30 +1000

    link fixes

--- packages/unstyled/react/src/components/file-upload/examples/with-field.tsx
+++ packages/unstyled/react/src/components/file-upload/examples/with-field.tsx
@@ -1,15 +1,17 @@
-import { Field } from '../../..'
-import { FileUpload } from '..'
+import { Field } from "../../.."
+import { FileUpload } from ".."
 
-export const WithField = (props: Field.RootProps) => (
-  <Field.Root {...props}>
-    <FileUpload.Root maxFiles={5}>
-      <FileUpload.Label>Label</FileUpload.Label>
-      <FileUpload.Trigger>Select</FileUpload.Trigger>
-      <FileUpload.ItemGroup />
-      <FileUpload.HiddenInput data-testid="input" />
-    </FileUpload.Root>
-    <Field.HelperText>Additional Info</Field.HelperText>
-    <Field.ErrorText>Error Info</Field.ErrorText>
-  </Field.Root>
-)
+export const WithField = (props: Field.RootProps) => {
+  return (
+    <Field.Root {...props}>
+      <FileUpload.Root maxFiles={5}>
+        <FileUpload.Label>Label</FileUpload.Label>
+        <FileUpload.Trigger>Select</FileUpload.Trigger>
+        <FileUpload.ItemGroup />
+        <FileUpload.HiddenInput data-testid="input" />
+      </FileUpload.Root>
+      <Field.HelperText>Additional Info</Field.HelperText>
+      <Field.ErrorText>Error Info</Field.ErrorText>
+    </Field.Root>
+  )
+}
